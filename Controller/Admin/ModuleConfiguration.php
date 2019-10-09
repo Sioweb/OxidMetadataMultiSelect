e73029cc242d9c659fcb6eb0748f22dc64eef38e
Sascha Weidner <=>
2019-10-09 12:41:32 +0200

    Reduce code

--- Controller/Admin/ModuleConfiguration.php
+++ Controller/Admin/ModuleConfiguration.php
@@ -18,31 +18,19 @@ class ModuleConfiguration extends ModuleConfiguration_parent
     {
         $sModuleId = $this->_sModuleId = $this->getEditObjectId();
 
+        $Template = parent::render();
         $oModule = oxNew(Module::class);
-
+        
         if ($sModuleId && $oModule->load($sModuleId)) {
             try {
                 $aDbVariables = $this->_loadMetadataConfVars($oModule->getInfo("settings"));
-
                 $this->_aViewData["var_settings"] = $aDbVariables['settings'];
-                $this->_aViewData["var_constraints"] = $aDbVariables['constraints'];
-                $this->_aViewData["var_grouping"] = $aDbVariables['grouping'];
-                $iCount = 0;
-                foreach ($this->_aConfParams as $sType => $sParam) {
-                    $this->_aViewData[$sParam] = $aDbVariables['vars'][$sType];
-                    $iCount += count($aDbVariables['vars'][$sType]);
-                }
             } catch (StandardException $oEx) {
                 Registry::getUtilsView()->addErrorToDisplay($oEx);
                 $oEx->debugOut();
             }
-        } else {
-            Registry::getUtilsView()->addErrorToDisplay(new StandardException('EXCEPTION_MODULE_NOT_LOADED'));
         }
-
-        $this->_aViewData["oModule"] = $oModule;
-
-        return 'module_config.tpl';
+        return $Template;
     }
 
     public function _loadMetadataConfVars($aModuleSettings)
@@ -51,8 +39,6 @@ class ModuleConfiguration extends ModuleConfiguration_parent
         $oConfig = $this->getConfig();
 
         $aSettings = [];
-
-        $aDbVariables = $this->loadConfVars($oConfig->getShopId(), $this->_getModuleForConfigVars());
 
         if (is_array($aModuleSettings)) {
             foreach ($aModuleSettings as $aValue) {
